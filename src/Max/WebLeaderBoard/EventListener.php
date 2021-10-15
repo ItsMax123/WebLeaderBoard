4a602956f3f9df86fa083c4cd5d4c50d1ce6ce84
ItsMax123 <43801744+ItsMax123@users.noreply.github.com>
2021-10-14 21:01:17 -0400

    v2

--- src/Max/WebLeaderBoard/EventListener.php
+++ src/Max/WebLeaderBoard/EventListener.php
@@ -1,86 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Max\WebLeaderBoard;
 
 use pocketmine\Player;
-use pocketmine\event\block\{BlockBreakEvent, BlockPlaceEvent};
 use pocketmine\event\Listener;
 
-use pocketmine\event\player\{PlayerJoinEvent, PlayerQuitEvent, PlayerDeathEvent, PlayerChatEvent};
+use pocketmine\event\block\{BlockBreakEvent, BlockPlaceEvent};
+use pocketmine\event\player\{PlayerJoinEvent, PlayerDeathEvent, PlayerChatEvent};
 use pocketmine\event\entity\{EntityDamageByEntityEvent};
-use pocketmine\event\plugin\PluginDisableEvent;
 use pocketmine\event\server\CommandEvent;
 
 
 class EventListener implements Listener {
     public function __construct($pl) {
         $this->plugin = $pl;
-        $pl->getServer()->getPluginManager()->registerEvents($this, $pl);
     }
-
-    public function onJoin(PlayerJoinEvent $event) {
-    	$playerName = $event->getPlayer()->getName();
-		$this->plugin->jointime[$playerName] = time();
-		$this->plugin->messages[$playerName] = "0";
-		$this->plugin->commands[$playerName] = "0";
-		$this->plugin->broken[$playerName] = "0";
-		$this->plugin->placed[$playerName] = "0";
-		$this->plugin->resetPlayerStats($playerName);
-		$this->plugin->setPlayerStats($playerName, "status", "Online");
-		$this->plugin->sendData();
-	}
 
 	public function onPlayerDeath(PlayerDeathEvent $event) {
 		$victim = $event->getPlayer();
-		$this->plugin->bumpPlayerStats($victim->getName(), "deaths", 1);
-		$this->plugin->setPlayerStats($victim->getName(), "killstreak", "0");
+		$this->plugin->bumpPageStat("Combat", $victim->getName(), "Deaths", 1);
+		$this->plugin->setPageStat("Combat", $victim->getName(), "Killstreak", "0");
 		if ($victim->getLastDamageCause() instanceof EntityDamageByEntityEvent) {
-			$this->plugin->bumpPlayerStats($victim->getLastDamageCause()->getDamager()->getName(), "kills", 1);
-			$this->plugin->bumpPlayerStats($victim->getLastDamageCause()->getDamager()->getName(), "killstreak", 1);
+			$this->plugin->bumpPageStat("Combat", $victim->getLastDamageCause()->getDamager()->getName(), "Kills", 1);
+			$this->plugin->bumpPageStat("Combat", $victim->getLastDamageCause()->getDamager()->getName(), "Killstreak", 1);
 		}
-		$this->plugin->sendData();
+	}
+
+	public function onJoin(PlayerJoinEvent $event) {
+    	$playerName = $event->getPlayer()->getName();
+    	foreach ($this->plugin->data["Pages"] as $pageName => $pageData) {
+    		if (!isset($this->plugin->data["Pages"][$pageName]["Players"][$playerName])) {
+    			$this->plugin->data["Pages"][$pageName]["Players"][$playerName] = $this->plugin->data["Pages"][$pageName]["default_layout"];
+			}
+		}
+    	if (!isset($this->plugin->data["Players"][$playerName])) $this->plugin->data["Players"][$playerName] = array("time_played" => "0");
 	}
 
 	public function onChat(PlayerChatEvent $event) {
-		++$this->plugin->messages[$event->getPlayer()->getName()];
+		$this->plugin->bumpPageStat("Index", $event->getPlayer()->getName(), "Messages", 1);
 	}
 
 	public function onCommand(CommandEvent $event) {
     	$playerName = $event->getSender();
-		if ($playerName instanceof Player) ++$this->plugin->commands[$playerName->getName()];
+		if ($playerName instanceof Player) $this->plugin->bumpPageStat("Index", $playerName->getName(), "Commands", 1);
 	}
 
 	public function onBlockBreak(BlockBreakEvent $event) {
-    	++$this->plugin->broken[$event->getPlayer()->getName()];
+		$this->plugin->bumpPageStat("Index", $event->getPlayer()->getName(), "Blocks Broken", 1);
 	}
 
 	public function onBlockPlace(BlockPlaceEvent $event) {
-		++$this->plugin->placed[$event->getPlayer()->getName()];
-	}
-
-	public function onLeave(PlayerQuitEvent $event) {
-    	$playerName = $event->getPlayer()->getName();
-		$this->plugin->setPlayerStats($playerName, "status", "Offline");
-		$this->plugin->bumpPlayerStats($playerName, "messages", $this->plugin->messages[$playerName]);
-		$this->plugin->bumpPlayerStats($playerName, "commands", $this->plugin->commands[$playerName]);
-		$this->plugin->bumpPlayerStats($playerName, "broken", $this->plugin->broken[$playerName]);
-		$this->plugin->bumpPlayerStats($playerName, "placed", $this->plugin->placed[$playerName]);
-		$this->plugin->bumpPlayerStats($playerName, "time_played", time() - $this->plugin->jointime[$playerName]);
-		$this->plugin->sendData(count($this->plugin->getServer()->getOnlinePlayers()) - 1);
-	}
-
-	public function onDisable(PluginDisableEvent $event) {
-    	if ($event->getPlugin()->getName() == "WebLeaderBoard") {
-			foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-				$playerName = $player->getName();
-				$this->plugin->setPlayerStats($playerName, "status", "Offline");
-				$this->plugin->bumpPlayerStats($playerName, "messages", $this->plugin->messages[$playerName]);
-				$this->plugin->bumpPlayerStats($playerName, "commands", $this->plugin->commands[$playerName]);
-				$this->plugin->bumpPlayerStats($playerName, "broken", $this->plugin->broken[$playerName]);
-				$this->plugin->bumpPlayerStats($playerName, "placed", $this->plugin->placed[$playerName]);
-				$this->plugin->bumpPlayerStats($playerName, "time_played", time() - $this->plugin->jointime[$playerName]);
-			}
-			$this->plugin->sendData(0);
-		}
+		$this->plugin->bumpPageStat("Index", $event->getPlayer()->getName(), "Blocks Placed", 1);
 	}
 }
